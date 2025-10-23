216b9fe81a71a5e8c07e915dd536dd07d74a059e
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-23 00:49:35 -0400

    Updated styling for pages

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,12 @@
+import styles from "../styles/Shop.module.css"
+
 export default function Shop() {
     return(
-        <main>
-            <h1>Shop</h1>
+        <main className={styles.container}>
+            <h1>OUR PRODUCTS</h1>
+            <div className={styles.products}>
+                
+            </div>
         </main>
     )
 }
