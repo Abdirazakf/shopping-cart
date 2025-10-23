9a3f2c0d3b8ce6816f8bea9c7e0de3b1be303e1a
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-22 21:21:56 -0400

    Updated components and styles

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,12 @@
+import styles from "../styles/Homepage.module.css"
+
 export default function Homepage() {
     return (
-        <h1>Homepage</h1>
+        <main className={styles.container} >
+            <h1>WELCOME TO OUR STORE</h1>
+            <p>Discover amazing products and start shopping
+                now! Click on Shop above to browse our collection
+            </p>
+        </main>
     )
 }
