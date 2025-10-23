bffb703c793166a98ea254ed505e52f43f605635
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-22 21:54:28 -0400

    Used useLocation hook to dynamically update the button bottom border

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,30 @@
-import { Link } from "react-router"
-import { useState } from "react"
+import { Link, useLocation } from "react-router"
 import cartLogo from '../assets/cart.svg'
 import styles from "../styles/NavBar.module.css"
 
 export default function NavBar() {
-    const [activePage, setActivePage] = useState(null)
-    
+    const location = useLocation()
+    console.log(location)
+
     return (
         <header className={styles.navHeader}>
             <img src={cartLogo} alt="Page Logo" />
             <h1>ShopMart</h1>
             <div className={styles.linkContainer}>
                 <Link to="/">
-                    <button>Home</button>
+                    <button className= {location.pathname === "/" ? styles.active : ""}>
+                        Home
+                    </button>
                 </Link>
                 <Link to="shop">
-                    <button>Shop</button>
+                    <button className= {location.pathname === "/shop" ? styles.active : ""}>
+                        Shop
+                    </button>
                 </Link>
                 <Link to="cart">
-                    <button>Cart</button>
+                    <button className= {location.pathname === "/cart" ? styles.active : ""}>
+                        Cart
+                    </button>
                 </Link>
             </div>
         </header>
