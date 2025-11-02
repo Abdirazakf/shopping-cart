59726914de604edf2c372f72633d2a140567ae74
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-02 15:16:31 -0500

    Removed unused library

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, BrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router";
 import App from './components/App'
 import Homepage from './components/Homepage'
 import Shop from './components/Shop'
